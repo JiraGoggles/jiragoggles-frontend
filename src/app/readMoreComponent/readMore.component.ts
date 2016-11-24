53e8919bedb6537e5d4ec36d968516bca09871f0
Mateusz Roszczyk <roszczyk93@gmail.com>
2016-11-24 17:10:37 +0100

    much better version of readmore component, some minor fixes todo like word boundary

--- src/app/readMoreComponent/readMore.component.ts
+++ src/app/readMoreComponent/readMore.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnChanges} from '@angular/core';
 
 @Component({
     selector: 'read-more',
     template: `
-        <div [class.collapsed]="isCollapsed">
-            <ng-content></ng-content>
-            <a href="#" (click)="isCollapsed =! isCollapsed">Read more</a>
+        <div [innerHTML]="currentText">
         </div>
-    `,
-    styles: [`
-        div.collapsed {
-            height: 250px;
-        }
-    `]
+            <a [class.hidden]="hideToggle" (click)="toggleView()">Read {{isCollapsed? 'more':'less'}}</a>
+    `
 })
 
-export class ReadMoreComponent {
-    isCollapsed: boolean = true;
+export class ReadMoreComponent implements OnChanges {
+    @Input() text: string;
+    @Input() maxLength: number = 100;
+    currentText: string;
+    hideToggle: boolean = true;
+
+    public isCollapsed: boolean = true;
+
+    constructor(private elementRef: ElementRef) {
+
+    }
+    toggleView() {
+        this.isCollapsed = !this.isCollapsed;
+        this.determineView();
+    }
+    determineView() {
+        if (this.text.length <= this.maxLength) {
+            this.currentText = this.text;
+            this.isCollapsed = false;
+            this.hideToggle = true;
+            return;
+        }
+        this.hideToggle = false;
+        if (this.isCollapsed == true) {
+            this.currentText = this.text.substring(0, this.maxLength) + "...";
+        } else if(this.isCollapsed == false)  {
+            this.currentText = this.text;
+        }
+
+    }
+    ngOnChanges() {
+        this.determineView();
+    }
 }
