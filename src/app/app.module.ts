b1ea383aec5fa023f9fd688f1918b5bf91dc4a62
Konrad Kruś <wiekonek@gmail.com>
2016-12-11 22:09:07 +0100

    Import ng2-pagination.
    - test basic pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import {ChildCardComponent} from "./card/child-card/child-card.component";
 import {ParentCardComponent} from "./card/parent-card/parent-card.component";
 import {EpicComponent} from "./cardsView/epic/epic.component";
 import {StoryComponent} from "./cardsView/story/story.component";
+import {Ng2PaginationModule} from 'ng2-pagination';
 
 
 // Application wide providers
@@ -61,6 +62,7 @@ type StoreType = {
   imports: [ // import Angular's modules
     BrowserModule,
     FormsModule,
+    Ng2PaginationModule,
     HttpModule,
     RouterModule.forRoot(ROUTES, { useHash: true })
   ],
