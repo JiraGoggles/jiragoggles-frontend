84f767c6e333e5050a6f4fdfd9fd514446150eb0
jjusianiec <jax1994@gmail.com>
2016-12-18 21:13:11 +0100

    Install yargs on heroku via npm

--- heroku-prebuild.sh
+++ heroku-prebuild.sh
@@ -1,9 +1,5 @@
-sudo apt-key adv --keyserver pgp.mit.edu --recv D101F7899D41F3C3
-echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update -qq
-sudo apt-get install -y -qq yarn
-sleep 3
 npm i -g npm@^3
+npm install -g yargs
 npm install -g webpack
 npm install -g webpack-dev-server
 npm install -g karma-cli
