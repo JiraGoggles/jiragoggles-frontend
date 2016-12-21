e217705131a4d170c93869731ff76234b273a843
Wojciech Pomykała <wojt.pomykal@gmail.com>
2016-12-21 23:37:45 +0100

    Fix 'get' method return type in cardsView services
    - update BaseCardMockService
    - update mocks (json files can be mapped directly to PaginateResponse<ParentCard> type)

--- src/app/cardsView/services/project/project.service.ts
+++ src/app/cardsView/services/project/project.service.ts
@@ -5,7 +5,6 @@ import {Injectable} from "@angular/core";
 import {Http, RequestOptions, URLSearchParams} from "@angular/http";
 import {ParentCard} from "../../../card/card";
 import {Observable} from "rxjs";
-import {CardsService} from "../cards-service";
 import {AppAuthenticationService} from "../../../app-authentication.service";
 import {BaseCardService} from "../base-card.service";
 import {PaginateResponse} from "../paginate-response";
@@ -16,8 +15,8 @@ export class ProjectService extends BaseCardService {
     super(http, pluginAuth);
   }
 
-  get(key: string): Observable<ParentCard[]> {
-    return this._get<ParentCard[]>('api/card/project/' + key);
+  get(key: string): Observable<PaginateResponse<ParentCard>> {
+    return this._get<ParentCard>('api/card/project/' + key);
   }
 
   getPage(page: number, size: number, key: string): Observable<PaginateResponse<ParentCard>> {
