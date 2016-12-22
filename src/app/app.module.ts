9bb91f9735e1ced4589751cb33b728bce091917e
Mateusz Roszczyk <roszczyk93@gmail.com>
2016-12-22 13:47:32 +0100

    added basic drag and drop for child cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import {DndModule} from 'ng2-dnd';
 import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
 
 
@@ -66,6 +67,7 @@ type StoreType = {
   imports: [ // import Angular's modules
     BrowserModule,
     FormsModule,
+    DndModule.forRoot(),
     Ng2PaginationModule,
     HttpModule,
     RouterModule.forRoot(ROUTES, { useHash: true })
