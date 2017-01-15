9ae47be2bd03a035ecf8c60e2a01b8d1e2eee9a5
Konrad Kruś <wiekonek@gmail.com>
2017-01-15 17:24:03 +0100

    Add status to child card.

--- src/app/card/parent-card/parent-card.component.ts
+++ src/app/card/parent-card/parent-card.component.ts
@@ -5,13 +5,6 @@ import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
 import {ParentCard} from "../card";
 import {ChildCardComponent} from "../child-card/child-card.component";
 
-enum StatusType {
-  TO_DO,
-  IN_PROGRESS,
-  DONE,
-  OTHER
-}
-
 @Component({
   selector: 'parent-card',
   templateUrl: './parent-card.component.html',
@@ -24,8 +17,6 @@ export class ParentCardComponent extends ChildCardComponent implements OnInit {
   @Output() onRankChangeRequest = new EventEmitter<string[]>();
   private parentPath: string;
   private path: string;
-  private statusType = StatusType;
-  private status: StatusType;
   private isOfProjectType: boolean; // for convenience sake (in templateUrl file)
 
   public onRankClick(id: string, direction: string){
@@ -40,25 +31,6 @@ export class ParentCardComponent extends ChildCardComponent implements OnInit {
       this.parentPath = 'project/' + this.model.key;
     else {
       this.parentPath = this.model.key;
-      if (this.model.status != null) {
-        switch (this.model.status.toUpperCase()) {
-          case "TO DO":
-            this.status = StatusType.TO_DO;
-            break;
-          case "IN PROGRESS":
-            this.status = StatusType.IN_PROGRESS;
-            break;
-          case "DONE":
-            this.status = StatusType.DONE;
-            break;
-          default:
-            this.status = StatusType.OTHER;
-            break;
-        }
-      }
-      else {
-        this.status = StatusType.OTHER;
-      }
     }
 
     // TODO get rid of this workaround eventually
