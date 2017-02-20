180d7cbb6d5952c8fa08f52cf0808e68254e7b68
Konrad Kruś <wiekonek@gmail.com>
2017-02-20 23:53:26 +0100

    Add jira instance name to last session key.

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -5,23 +5,25 @@ import {Injectable} from "@angular/core";
 import {Router, NavigationEnd} from "@angular/router";
 import 'rxjs/add/operator/take';
 
-const sessionKeyword: string = "session";
-
+const sessionKeyword: string = "-last_session";
 @Injectable()
 export class SessionService  {
 
+  public JiraInstance = "";
+
   constructor(private router: Router) {
+    this.JiraInstance = document.location.href.split('/')[2];
+    console.log(this.JiraInstance);
   }
 
   public startSessionUrlUpdate(): void {
     this.router.events
       .filter(e => e instanceof NavigationEnd)
-      .subscribe(e => localStorage.setItem(sessionKeyword, e.url));
+      .subscribe(e => localStorage.setItem(this.JiraInstance + sessionKeyword, e.url));
   }
 
-
   public loadLastSession(): void {
-    let lastPath = localStorage.getItem(sessionKeyword);
+    let lastPath = localStorage.getItem(this.JiraInstance + sessionKeyword);
     this.router.events
       .take(1)
       .subscribe(_ => this.router.navigate(lastPath == null ? [''] : [lastPath]));
